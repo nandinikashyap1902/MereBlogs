f31c346194bdc995b7315d39e913b7c65caee2b9
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-09-06 19:26:36 +0530

    app initialiazed

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="/" className="logo">MyBlog</a>
+        <nav>
+          <a href="/login" >Login</a>
+          <a href="/signUp" >Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+        <img src="https://www.marthastewart.com/thmb/qq94YZhTQ4zz7jwRbkn4OfUb4Tc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/blooming-azalea-plant-getty-1221-2000-65bfc3a5812c4286a2c22aeeddd491f6.jpg" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>How flowers are important in our life?</h2>
+          <p className="info">
+            <a className="author">nandini</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+        <p className="summary">Many flowers are possible to eat, and they have fed and sweetened many generations. The flowers form an integral part of many foods, such as salads and soaps, and are also used in jellies, jams, wine, and even tea production.</p>
+          </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://www.marthastewart.com/thmb/qq94YZhTQ4zz7jwRbkn4OfUb4Tc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/blooming-azalea-plant-getty-1221-2000-65bfc3a5812c4286a2c22aeeddd491f6.jpg" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>How flowers are important in our life?</h2>
+          <p className="info">
+            <a className="author">nandini</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+        <p className="summary">Many flowers are possible to eat, and they have fed and sweetened many generations. The flowers form an integral part of many foods, such as salads and soaps, and are also used in jellies, jams, wine, and even tea production.</p>
+          </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://www.marthastewart.com/thmb/qq94YZhTQ4zz7jwRbkn4OfUb4Tc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/blooming-azalea-plant-getty-1221-2000-65bfc3a5812c4286a2c22aeeddd491f6.jpg" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>How flowers are important in our life?</h2>
+          <p className="info">
+            <a className="author">nandini</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+        <p className="summary">Many flowers are possible to eat, and they have fed and sweetened many generations. The flowers form an integral part of many foods, such as salads and soaps, and are also used in jellies, jams, wine, and even tea production.</p>
+          </div>
+      </div>
+      
+   </main>
+  );
+}
+
+export default App;
