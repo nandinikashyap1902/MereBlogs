d7e92676b6a8345f71de32303ed69a8fae20e1df
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-09-16 20:27:46 +0530

    node updated

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
-app.get('/test', (req, res) => {
-    res.json('test ok')
+const mongoose = require("mongoose")
+const User = require('./models/User')
+app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb://localhost:27017/User",{
+}).then(() => {
+    console.log('MongoDB connection established successfully.');
+})
+.catch((error) => {
+    console.error('MongoDB connection failed:', error);
+});
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
+    const userDoc = User.create({username,password})
+    // res.json({requestData:{username,password}})
+     res.json(userDoc)
 })
 
 app.listen(4000)
--- client/src/RegisterPage.js
+++ client/src/RegisterPage.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import "./App.css"
 export default function RegisterPage() {
-  const[username,setUsername]=useState('')
-  const[password,setPassword]=useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch('http://localhost:4000/register', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers:{'Content-Type':'application/json'}
+     })
+  }
+
+
   return (
-      <form className='register'>
+      <form className='register' onSubmit={register}>
           <h1>Register</h1>
-    <input type='text' placeholder='username'/> 
-    <input type='password' placeholder='password'/> 
+    <input type='text' placeholder='username' onChange={ev=>setUsername(ev.target.value)} value={username}/> 
+    <input type='password' placeholder='password' onChange={ev=>setPassword(ev.target.value)} value={password}/> 
     <button>Register</button>
 </form>
   )
