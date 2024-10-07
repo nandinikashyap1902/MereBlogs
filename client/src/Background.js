cfb61039c255f467a65d91f6da41ee8595309f81
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-10-07 18:47:07 +0530

    making background responsive

--- client/src/Background.js
+++ client/src/Background.js
@@ -20,61 +20,35 @@ function Background() {
           {/* <Lottie className='footerAni' animationData={footerAnimation}></Lottie> */}
             </div>
             )}
-      <div classname="bg-box">
+      <div className='bg-container'>
+
+    
         <Lottie animationData={background} className="lottie-animation"></Lottie>
         
-          <div className="laptopMan">
-              <Lottie animationData={man} className="man"></Lottie>
+          <div className="Back-laptopMan">
+              <Lottie animationData={man} className="laptopman"></Lottie>
               <div className="text">
-              <h1 style={{fontSize:'40px'}} className='typing-text'>WELCOME TO THIS SITE!!</h1>
-                  <h3>A free Blog creation place</h3>
-                  <p>Share your thoughts,and experiences!!</p>
+              <h1 style={{fontSize:'40px'}} className='typing-text'>Welcome to MereBlogs</h1>
+<h4>Your Ultimate Blogging Playground</h4>
+<p>Unleash your creativity! Share your insights, stories, and passions.</p>
+<p>Join us today and start your blogging journey!</p>
+
               </div>
         </div>
         
-        <div className="laptopMan man2">
+        <div className="Back-MobileMan">
         <div className="text">
-              <h1>Just create and post</h1>
-                  <h3>A blog (a truncation of "weblog") is an informational website consisting of discrete, often informal diary-style text entries (posts).</h3>
-                  <p>A blog (a truncation of "weblog") is an informational website co</p>
-              </div>
-          <Lottie animationData={MobileMan} className='man'></Lottie>
-          
-        </div>
-        <div class="typing-text">
-          <h1>Press down to show your creations.</h1></div>
-          <div className="post-container">
-              <Lottie animationData={arrow} className="arrow"></Lottie>
-          {/* <button onClick={toggleNavbar}> {showNavbar ? 'Hide Navbar' : 'Show Navbar'}</button> */}
-          <div class="buttons">
-  <button class="blob-btn" onClick={toggleNavbar}>
-  {showNavbar ? 'POST' : 'Show Navbar'}
-    <span class="blob-btn__inner">
-      <span class="blob-btn__blobs">
-        <span class="blob-btn__blob"></span>
-        <span class="blob-btn__blob"></span>
-        <span class="blob-btn__blob"></span>
-        <span class="blob-btn__blob"></span>
-      </span>
-    </span>
-  </button>
-  <br/>
+        <h1 style={{fontSize:'40px'}} className='typing-text'>Discover Your Voice!</h1>
 
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-  <defs>
-    <filter id="goo">
-      <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
-      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
-      <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
-    </filter>
-  </defs>
-              </svg>
+<p>Join us today and start your blogging journey!</p>
               </div>
-          </div>
+          <Lottie animationData={MobileMan} className='mobileman'></Lottie>
+        </div>
+       </div>
           <footer >
               <Lottie  id="footer" animationData={background}></Lottie>
           </footer>
-        </div>
+    
         </>
   )
 }
