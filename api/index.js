a13d320c6197f5b77b9876dc8a0e1db283dfb606
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-10-09 18:42:07 +0530

    mongodb url updated

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const mongoURI="mongodb+srv://NKashyap23:<db_password>@cluster0.mxgfz.mongodb.net/"
+const mongoURI="mongodb+srv://nandinikashyap:Nkashyap123@cluster0.mxgfz.mongodb.net/"
 const mongoose = require("mongoose")
 const User = require('./models/User')
 const bcrypt = require('bcrypt')
